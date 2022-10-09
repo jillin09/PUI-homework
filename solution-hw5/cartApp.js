666f3d78d5a39e5e18183423d83822ebbf08052b
jillin09 <109121677+jillin09@users.noreply.github.com>
2022-10-08 22:32:28 -0400

    hw5 update

---
+++ solution-hw5/cartApp.js
@@ -0,0 +1,140 @@
+// Remove button function. delete both graphical representation and data
+function deleteElement(newOne) {
+  newOne.element.remove();
+  newRollset.delete(newOne);
+}
+
+
+//information (roll type, glazing, pack size, base price) into an instance of the class Roll
+
+class Roll {
+    constructor(rollType, rollGlazing, packSize, basePrice) {
+        this.type = rollType;
+        this.glazing =  rollGlazing;
+        this.size = packSize;
+        this.basePrice = basePrice;
+    }
+}
+
+//Create a set to represent my cart.
+let newRollset = new Set();
+
+//Create a function to add new roll to the set
+function newRolls(rollType, rollGlazing, packSize, basePrice){
+  const newOne = new Roll(rollType, rollGlazing, packSize, basePrice);
+  newRollset.add(newOne);
+  return newOne;
+}
+
+//code roll data provided in the assignment
+const roll1 = newRolls('Original', 'Sugar Milk', '1', rollop["Original"].basePrice);
+const roll2 = newRolls('Walnut', 'Vanilla Milk', '12', rollop["Walnut"].basePrice);
+const roll3 = newRolls('Raisin', 'Sugar Milk', '3', rollop["Raisin"].basePrice);
+const roll4 = newRolls('Apple', 'Orignal', '3', rollop["Apple"].basePrice);
+
+
+//function to calculate total price in the shopping cart which can also be used to update price
+function totalPrice(){
+  let ttPrice = 0;
+
+  //pack size change
+  for (let i of newRollset){
+    let price = i.basePrice;
+    let pack = i.size;
+    if (pack == 6) {
+      pack = 5;
+    } else if (pack == 12) {
+      pack = 10;
+    } else {
+      pack = pack; 
+    }
+    let glz = i.glazing;
+
+    //glazing price change
+    if (glz == "Vanilla Milk") {
+      price = price+0.5;
+    } else if (glz == "Double Chocolate") {
+      price = price+1.5;
+    } else {
+      price = price;
+    }
+  let total = price*pack;
+  
+  ttPrice = ttPrice+total;
+  }
+
+  //total price to two digits
+ return ttPrice.toFixed(2);
+
+}
+
+//function to make use of template tag, update price, and make functional delete button
+function newCartItem(newOne) {
+
+  const tempCart = document.querySelector('#cartTemplate').content;  
+  const copyCart = document.importNode(tempCart,true);
+  newOne.element = copyCart.querySelector(".onecontainer"); //actual card containter
+  const wholeList = document.querySelector('.cart-item'); //whole shopping cart div (multiple cards)
+  wholeList.append(newOne.element); //update the shopping cart with items inside the set
+  const btnDelete  = newOne.element.querySelector('.remove'); //get remove button from DOM
+  btnDelete .addEventListener('click',()=>{  
+    deleteElement(newOne);  
+    const totalElement = document.querySelector(".priceTotal");
+    totalElement.innerText = "$ "+totalPrice(); //update the recalculate the total price
+    })
+  changeTextimg(newOne); //update text and image description of each item with rollop data js
+  
+}
+
+//construct html div using template for each item
+for (const newOne of newRollset){
+  newCartItem(newOne);
+}
+
+function changeTextimg(newOne){
+
+  const imgElement = newOne.element.querySelector(".cart-thumbnail");
+  const packElement = newOne.element.querySelector("#packChoice");
+  const glazeElement = newOne.element.querySelector("#glazChoice");
+  const nameElement = newOne.element.querySelector("#nameChoice");
+  const priceElement = newOne.element.querySelector(".price");
+
+  imgElement.src =rollop[newOne.type].imageFile;
+  nameElement.innerText = newOne.type ;
+  glazeElement.innerText =  newOne.glazing;
+  packElement.innerText = newOne.size;
+
+  let basePricenow  = newOne.basePrice;
+  let packnow  = newOne.size;
+
+  //pack size change
+
+  if (packnow == 6) {
+    packnow = 5;
+  } else if (packnow == 12) {
+    packnow = 10;
+  } else {
+    packnow = packnow;
+  }
+  let nowGlaze  = newOne.glazing;
+
+  //glazing price change
+
+  if (nowGlaze == "Vanilla Milk") {
+    basePricenow = basePricenow+0.5;
+  } else if (nowGlaze == "Double Chocolate") {
+    basePricenow = basePricenow+1.5;
+  } else {
+    basePricenow = basePricenow;
+  }
+
+  let packPrice = packnow*basePricenow;
+  priceElement.innerText = "$ "+packPrice.toFixed(2);
+}
+
+
+const totalElement = document.querySelector(".priceTotal");
+totalElement.innerText = "$ "+totalPrice();
+
+const queryString = window.location.search;
+const params = new URLSearchParams(queryString);
